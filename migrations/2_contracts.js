db20c560b3f10a2f1b7257f529b5eb53d3f21ad6
DonTseTse <blockchain-bootcamp@ts.hydramail.agerio.com>
2021-11-15 02:15:51 +0100

    Adapting migrations to be able to have the UoA address ready for the EscrowPaymentSplitter constructor call

--- migrations/2_contracts.js
+++ migrations/2_contracts.js
@@ -1,7 +1,8 @@
 const UoA = artifacts.require("UoA");
 const EscrowPaymentSplitter = artifacts.require("EscrowPaymentSplitter");
 
-module.exports = function (deployer) {
-    deployer.deploy(UoA);
-    deployer.deploy(EscrowPaymentSplitter);
+module.exports = async function (deployer) {
+    await deployer.deploy(UoA);
+    let uoa = await UoA.deployed();
+    await deployer.deploy(EscrowPaymentSplitter, uoa.address);
 };  
